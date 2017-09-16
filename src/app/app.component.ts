ed8ee050acff4bd4a254226ea3872bc189bdb2b1
Alex Knowlton <alexrules313@gmail.com>
2017-09-16 04:13:30 -0400

    adding homepage jazz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/map';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  appName = 'GradeViz';
+	departmentCtrl: FormControl;
+	filteredStates: Observable<any[]>;
+
+	departments: any[] = [
+	{
+		name: 'Arkansas',
+		number: '2.978M'
+	},
+	{
+		name: 'California',
+		number: '39.14M'
+	},
+	{
+		name: 'Florida',
+		number: '20.27M'
+	},
+	{
+		name: 'Texas',
+		number: '27.47M'
+	}
+	];
+	
+	appName = 'GradeViz';
+
+	constructor() {
+		this.departmentCtrl = new FormControl();
+		this.filteredStates = this.departmentCtrl.valueChanges
+		.startWith(null)
+		.map(department=> department? this.filterStates(department) : this.departments.slice());
+	}
+
+	filterStates(name: string) {
+		return this.departments.filter(department=>
+			department.name.toLowerCase().indexOf(name.toLowerCase()) === 0);
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MdToolbarModule } from '@angular/material';
+import { MdAutocompleteModule } from '@angular/material';
+import { MdInputModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -13,7 +16,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MdToolbarModule
+    MdToolbarModule,
+    MdAutocompleteModule,
+    MdInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
