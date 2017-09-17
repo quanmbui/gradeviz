7b2a210fca5bdc2604779571a6252dc34cba290b
Alex Knowlton <alexrules313@gmail.com>
2017-09-16 20:58:27 -0400

    stylized home page

--- src/services/courses.ts
+++ src/services/courses.ts
@@ -11,39 +11,57 @@ import { SemesterEnum } from '../util/semesterEnum';
 export class CourseDataSource extends DataSource<any> {
   courseResults: FirebaseListObservable<Course[]>;
 
+  departmentSubject: Subject<string>;
   nameSubject: Subject<string>;
   semesterSubject: Subject<SemesterEnum>;
 
-  hasData: boolean;
+  orderByChild: string;
 
   constructor(db: AngularFireDatabase) {
     super();
 
-    this.nameSubject = new Subject();
-    this.semesterSubject = new Subject();
-    
-    this.courseResults = db.list('/evals/' + this.semesterSubject, {
+    this.orderByChild = 'department'
+
+    this.courseResults = db.list('/evals/' + (this.semesterSubject ? this.semesterSubject : ''), {
       query: {
-        orderByChild: 'Name',
+        orderByChild: 'Department',
         equalTo: this.nameSubject
       }
     });
 
-    this.courseResults.subscribe(list => list.map(item => console.log(item)));
+    this.departmentSubject = new Subject();
+    this.nameSubject = new Subject();
+    this.semesterSubject = new Subject();
 
-    this.hasData = true;
+    this.courseResults.subscribe(list => list.map(item => console.log(item)));
   }
 
-  connect(): Observable<Course[]> {
+  public connect(): Observable<Course[]> {
     return this.courseResults;
   }
 
-  search(department: string, courseName: string, isWritingIntensive: boolean, courseDesignation: string, semester: SemesterEnum): void {
+  public search(department?: string, courseName?: string, semester?: SemesterEnum): void {
     console.log("getting data for semester " + SemesterEnum[semester]);
+    semester = semester ? semester : SemesterEnum[SemesterEnum[12]]
 
+    if(department) {
+      this.searchByDepartment(department, semester)
+    } else if (courseName) {
+      this.searchByName(courseName, semester)
+    } else {
+      console.log("ERROR SEARCHING")
+    }
+  }
+
+  public disconnect() {}
+
+  private searchByName(courseName: string, semester: SemesterEnum): void {
     this.semesterSubject.next(semester);
     this.nameSubject.next(courseName);
   }
 
-  disconnect() {}
+  private searchByDepartment(deparment: string, semester: SemesterEnum): void {
+    this.semesterSubject.next(semester);
+    this.departmentSubject.next(deparment);
+  }
 }
